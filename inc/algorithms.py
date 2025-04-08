6c10ab7f7f2b34d3e7ed6c80367b28e6bd63b938
PlumLulz <plumlulz@gmail.com>
2025-04-07 21:41:39 -0600

    Updated valid algorithm list

--- inc/algorithms.py
+++ inc/algorithms.py
@@ -183,5 +183,6 @@ validalgs = {
     "28200": "Exodus Desktop Wallet (scrypt)",
     "28800": "Kerberos 5, etype 17, DB",
     "28900": "Kerberos 5, etype 18, DB",
+    "99829": ".DMG Encryption",
     "99849": "Yescrypt $y$"
 }
